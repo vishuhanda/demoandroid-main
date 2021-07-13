77d6334ed7f30998916acb2ea608071ad623fa60
vishuhanda <49567970+vishuhanda@users.noreply.github.com>
2021-07-13 15:12:58 +0530

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -4,28 +4,46 @@ echo "**************************************************************************
 appName="vishuhanda001-gmail.com/demoandroid-appcenter"
 deviceSetName="itsvishuhanda-gmail.com/demo_one_device"
 testSeriesName="AppiumTestSeries"
-# token="de6c86f9a15d5644fe7512541d1c181bfd9c1d17"
 token="588ee653c54ef42ba9204951ddbced2fddd0f69b"
 buildDir="target/upload"
-appcenter = $APPCENTER_OUTPUT_DIRECTORY
-echo "appcenteroutput" $APPCENTER_OUTPUT_DIRECTORY
 
+
+
+echo "Source directory: $APPCENTER_SOURCE_DIRECTORY"
+echo "Output directory: $APPCENTER_OUTPUT_DIRECTORY/*.apk"
+echo "        App Name: $appName"
+echo "      Device Set: $deviceSetName"
+echo "     Test Series: $testSeriesName"
+echo "       API_token: $token"
+echo "       API_token: $buildDir"
+echo
 
 
 
 echo "cloning repo"
-git clone https://github.com/vishuhanda/browserstacknewappium.git
+git clone https://github.com/vishuhanda/appiumappcenter.git
 echo "cloning done"
-cd browserstacknewappium
+cd appiumappcenter
 echo "navigated to directory"
-echo "running test cases"
-mvn clean install -Dsuitexml=testng.xml -Dbrowserstackurl=bs://08760d3f8668615030d376a1f755544a4e027df3
-if [ $? -eq 0 ]; then
-    echo "Maven Build successfully made"
-    echo "distributing app"
-    appcenter distribute release --app $appName --file $APPCENTER_OUTPUT_DIRECTORY/*.apk --group "betatesters" --token $token
-    echo "app distributed"
-else
-    echo "Build Failed"
-fi
+echo "navigated to directory"
+mvn -DskipTests -P prepare-for-upload package 
+echo "uploading test cases"
+echo "Running app center"
+appcenter test run appium --app  $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series $testSeriesName  --locale "en_US" --token $token --build-dir $PWD/target/upload
+
+
+appcenter distribute release --app $appName --file $APPCENTER_OUTPUT_DIRECTORY/*.apk --group "betatesters" --token $token
+
+
+
+
+# mvn clean install -Dsuitexml=testng.xml 
+# if [ $? -eq 0 ]; then
+#     echo "Maven Build successfully made"
+#     echo "distributing app"
+#     appcenter distribute release --app $appName --file $APPCENTER_OUTPUT_DIRECTORY/*.apk --group "betatesters" --token $token
+#     echo "app distributed"
+# else
+#     echo "Build Failed"
+# fi
 
