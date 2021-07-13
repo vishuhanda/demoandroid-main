9a029df5d3c6a09acbaa936289b461f6d47ce0cc
vishuhanda <49567970+vishuhanda@users.noreply.github.com>
2021-07-13 14:30:55 +0530

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -7,6 +7,9 @@ testSeriesName="AppiumTestSeries"
 # token="de6c86f9a15d5644fe7512541d1c181bfd9c1d17"
 token="588ee653c54ef42ba9204951ddbced2fddd0f69b"
 buildDir="target/upload"
+appcenter = $APPCENTER_OUTPUT_DIRECTORY
+echo appcenteroutput appcenter
+
 
 
 
@@ -16,7 +19,7 @@ echo "cloning done"
 cd browserstacknewappium
 echo "navigated to directory"
 echo "running test cases"
-mvn clean install -Dsuitexml=testng.xml -Dbrowserstackurl=bs://08760d3f8668615030d376a1f755544a4e027df3
+# mvn clean install -Dsuitexml=testng.xml -Dbrowserstackurl=bs://08760d3f8668615030d376a1f755544a4e027df3
 if [ $? -eq 0 ]; then
     echo "Maven Build successfully made"
     echo "distributing app"
